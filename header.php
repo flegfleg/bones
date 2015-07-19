98897f2b43c2f232c89035747bf09e384801bc79
Florian Egermann <florian@macht-medien.de>
2015-07-19 19:15:16 +0200

    re-added navigation

--- header.php
+++ header.php
@@ -62,13 +62,29 @@
 
             </div>
 
-            <div class="bit-1">
-            <div class="inner">2nd</div>
-            </div>            
-            <div class="bit-1">
-            3rd
-            </div>
+            <div class="bit-2">
 
+              <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+              <!-- responsive nav: navigataur -->
+              <input type="checkbox" id="toggle" />
+              <label for="toggle" class="toggle" onclick></label>
+
+                <?php wp_nav_menu(array(
+                  'container' => false,                           // remove nav container
+                  'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                  'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+                  'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                  'theme_location' => 'main-nav',                 // where it's located in the theme
+                  'before' => '',                                 // before the menu
+                  'after' => '',                                  // after the menu
+                  'link_before' => '',                            // before each link
+                  'link_after' => '',                             // after each link
+                  'depth' => 0,                                   // limit the depth of the nav
+                  'fallback_cb' => ''                             // fallback function (if there is one)
+                )); ?>
+              </nav>
+
+            </div>            
 
           </div>
 
