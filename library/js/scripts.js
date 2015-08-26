44f09d6c24c32d06018c0db42f45e77ef55f5f08
Florian Egermann <florian@macht-medien.de>
2015-08-26 15:47:48 +0200

    added doc link & removed comments

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,28 +1,20 @@
 /*
  * Bones Scripts File
- * Author: Eddie Machado
+ * Author: Florian Egermann
  *
- * This file should contain any js scripts you want to add to the site.
- * Instead of calling it in the header or throwing it inside wp_head()
- * this file will be called automatically in the footer so as not to
- * slow the page load.
- *
- * There are a lot of example functions and tools in here. If you don't
- * need any of it, just remove it. They are meant to be helpers and are
- * not required. It's your world baby, you can do whatever you want.
 */
-
-
 
 
 jQuery(document).ready(function($) {
 
   // add lightbox to wordpress galleries
+  // see: http://dimsemenov.com/plugins/magnific-popup/documentation.html
+
   $('.gallery-icon').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
     gallery:{enabled:true}
-});
+  });
 
 
 }); /* end of as page load scripts */
